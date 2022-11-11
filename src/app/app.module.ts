a10910b50d835b552a8eebab6d0fcb055ec3c31c
Saull Brandão <saull@outlook.com>
2022-11-11 11:12:34 -0300

    populate form fields with setValue and patchValue

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { HttpClientModule } from '@angular/common/http';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
@@ -10,7 +10,13 @@ import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [AppComponent, TemplateFormComponent, DataFormComponent],
-  imports: [BrowserModule, AppRoutingModule, NgbModule, FormsModule],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    NgbModule,
+    FormsModule,
+    HttpClientModule,
+  ],
   providers: [],
   bootstrap: [AppComponent],
 })
--- src/app/template-form/template-form.component.ts
+++ src/app/template-form/template-form.component.ts
@@ -1,5 +1,7 @@
+import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
-import { NgForm } from '@angular/forms';
+import { NgForm, NgModel } from '@angular/forms';
+import { map } from 'rxjs';
 
 @Component({
   selector: 'app-template-form',
@@ -12,10 +14,42 @@ export class TemplateFormComponent implements OnInit {
     email: null,
   };
 
-  constructor() {}
+  constructor(private http: HttpClient) {}
 
   onSubmit(form: NgForm) {
     console.log(form);
+  }
+
+  isInvalid(input: NgModel) {
+    return input.invalid && input.touched;
+  }
+
+  getAddressByCep(cep: string, form: NgForm) {
+    const formattedCEP = cep.replace(/\D/g, '');
+
+    if (formattedCEP !== '') {
+      const cepValidation = /^[0-9]{8}$/;
+
+      if (cepValidation.test(formattedCEP)) {
+        this.http
+          .get(`https://viacep.com.br/ws/${formattedCEP}/json`)
+          .pipe(map((data) => data))
+          .subscribe((data) => this.populateForm(data, form));
+      }
+    }
+  }
+
+  populateForm(data: any, form: NgForm) {
+    form.form.patchValue({
+      endereco: {
+        rua: data.logradouro,
+        cep: data.cep,
+        bairro: data.bairro,
+        cidade: data.localidade,
+        estado: data.uf,
+        complemento: data.complemento,
+      },
+    });
   }
 
   ngOnInit(): void {}
