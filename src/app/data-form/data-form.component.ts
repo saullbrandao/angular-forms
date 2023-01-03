c2737a9dc9d4c52bbe1724fb170b2d6f96587663
Saull Brandão <saull@outlook.com>
2023-01-03 08:30:15 -0300

    migrate from template to data driven form

--- src/app/data-form/data-form.component.ts
+++ src/app/data-form/data-form.component.ts
@@ -1,3 +1,4 @@
+import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
 import {
   FormBuilder,
@@ -5,6 +6,7 @@ import {
   FormGroup,
   Validators,
 } from '@angular/forms';
+import { map } from 'rxjs';
 
 @Component({
   selector: 'app-data-form',
@@ -14,7 +16,7 @@ import {
 export class DataFormComponent implements OnInit {
   form: FormGroup;
 
-  constructor(private formBuilder: FormBuilder) {
+  constructor(private formBuilder: FormBuilder, private http: HttpClient) {
     // this.form = new FormGroup({
     //   name: new FormControl(),
     //   email: new FormControl(),
@@ -22,6 +24,15 @@ export class DataFormComponent implements OnInit {
     this.form = this.formBuilder.group({
       name: [null, Validators.required],
       email: [null, [Validators.required, Validators.email]],
+      address: this.formBuilder.group({
+        cep: [null, Validators.required],
+        number: [null, Validators.required],
+        street: [null, Validators.required],
+        complement: [null],
+        district: [null, Validators.required],
+        city: [null, Validators.required],
+        state: [null, Validators.required],
+      }),
     });
   }
 
@@ -32,6 +43,40 @@ export class DataFormComponent implements OnInit {
   }
 
   onSubmit() {
-    console.log(this.form);
+    if (this.form.invalid) {
+      Object.keys(this.form.controls).forEach((field) => {
+        const control = this.form.get(field);
+        control?.markAllAsTouched();
+      });
+    }
+  }
+
+  getAddressByCep() {
+    const cep = this.form.get('address.cep')?.value;
+    const formattedCEP = cep.replace(/\D/g, '');
+
+    if (formattedCEP !== '') {
+      const cepValidation = /^[0-9]{8}$/;
+
+      if (cepValidation.test(formattedCEP)) {
+        this.http
+          .get(`https://viacep.com.br/ws/${formattedCEP}/json`)
+          .pipe(map((data) => data))
+          .subscribe((data) => this.populateForm(data));
+      }
+    }
+  }
+
+  populateForm(data: any) {
+    this.form.patchValue({
+      address: {
+        street: data.logradouro,
+        cep: data.cep,
+        district: data.bairro,
+        city: data.localidade,
+        state: data.uf,
+        complement: data.complemento,
+      },
+    });
   }
 }
